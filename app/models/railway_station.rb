309d9b25bbb6b24d6bcd6f1eb4330895c3bfdade
disne_y <bold.disney@gmail.com>
2017-03-18 15:28:35 +0300

    Add associations with tickets (departure and arrival)

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,6 +1,7 @@
 class RailwayStation < ActiveRecord::Base
   has_many :trains
-  has_many :tickets
+  has_many :tickets_departure, class_name: 'Ticket', foreign_key: :start_station_id
+  has_many :tickets_arrival, class_name: 'Ticket', foreign_key: :end_station_id
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   
