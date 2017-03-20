a7a0742b94fd3dff349cbbe241601b74a53d6757
disne_y <bold.disney@gmail.com>
2017-03-20 13:37:27 +0300

    Add default_position to the railway_stations_routes

---
+++ db/migrate/20170319140918_add_number_of_stations_to_the_route.rb
@@ -0,0 +1,6 @@
+class AddNumberOfStationsToTheRoute < ActiveRecord::Migration
+  def change
+    add_column :railway_stations_routes, :id, :primary_key
+    add_column :railway_stations_routes, :position, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319131903) do
+ActiveRecord::Schema.define(version: 20170319140918) do
 
   create_table "carriages", force: :cascade do |t|
     t.string   "category"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20170319131903) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "railway_stations_routes", id: false, force: :cascade do |t|
-    t.integer "railway_station_id", null: false
-    t.integer "route_id",           null: false
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id",             null: false
+    t.integer "route_id",                       null: false
+    t.integer "position",           default: 0
   end
 
   add_index "railway_stations_routes", ["railway_station_id"], name: "index_railway_stations_routes_on_railway_station_id"
