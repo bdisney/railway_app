82e89d8522d9128fc018e80cc66843b6a6e7354e
disne_y <bold.disney@gmail.com>
2017-03-20 18:18:41 +0300

    Add carriage types, seats types. Also add set_number method. Also add scopes.

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,17 +1,36 @@
 class Carriage < ActiveRecord::Base
+  self.inheritance_column = :foo
+
+  TYPES = { CoupeCarriage: 'Coupe', EconomyCarriage: 'Economy', BusinessClassCarriage: 'Business - Class', SittingCarriage: 'Sitting' }
+  SEATS_TYPES = { bottom_seats: 'Bottom', top_seats: 'Top', side_bottom_seats: 'Side bottom',
+                  side_top_seats: 'Side top', seats: 'Seats' }
   belongs_to :train
-
-  CATEGORIES = { coupe: 'Coupe', economy: 'Econom-class'}.freeze
-
+  
   validates :train_id, presence: true
-  validates :category, inclusion: { in: %w( coupe economy ) }
-  validates :top_seats, :bottom_seats, numericality: { only_integer: true, greater_than: 0 }
+  validates :number, numericality: { only_integer: true, greater_than: 0 }
+  validates :number, uniqueness: { scope: :train_id }
 
-  def self.categories 
-    CATEGORIES.invert
+  before_validation :set_number, on: :create
+
+  scope :ordered, -> { order(:number) }
+  scope :ordered_by_train, -> { joins(:train).order('trains.number, carriages.number') }
+
+  def self.types
+    TYPES.invert
   end
 
-  def category_label
-    CATEGORIES.fetch(category.to_sym)
+  def self.permited_params
+    [:type, :train_id, :number]
+  end
+
+  def type_label
+    TYPES.fetch(type.to_sym)
+  end
+
+  protected
+
+  def set_number
+    max_number = train.carriages.maximum(:number) || 0
+    self.number = max_number + 1
   end
 end
