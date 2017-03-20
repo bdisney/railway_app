e5b779a40b41e309d01985675ac75344c87e6936
disne_y <bold.disney@gmail.com>
2017-03-20 13:44:39 +0300

    Add order_stations, stations_count and set_name methods to route model

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,5 +3,25 @@ class Route < ActiveRecord::Base
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
 
+  before_validation :set_name
+  before_create :order_stations
+
   validates :name, presence: true
+  validate :stations_count
+
+  private
+
+  def set_name
+    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
+
+  def order_stations
+    railway_stations_routes.each_with_index do |station, position|
+      station.position = position
+    end
+  end
+
+  def stations_count
+    errors.add(:base, 'Route should contain at least 2 stations') if railway_stations.size < 2
+  end
 end
