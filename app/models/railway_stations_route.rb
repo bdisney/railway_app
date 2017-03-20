f4d2aa853b4d4087f2e8cd6e66e8b7ea6f328d10
disne_y <bold.disney@gmail.com>
2017-03-20 13:40:00 +0300

    Add validation for station_position and railway_station_id

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,4 +1,8 @@
 class RailwayStationsRoute < ActiveRecord::Base
   belongs_to :railway_station
   belongs_to :route
+
+  validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
+  
+  validates :railway_station_id, uniqueness: { scope: :route_id }
 end
