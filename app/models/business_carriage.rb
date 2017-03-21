6d7570e45eef26fbe87f3df8b68cce8f3091b843
disne_y <bold.disney@gmail.com>
2017-03-21 14:27:03 +0300

    Update views. Rename business_class_carriage

--- app/models/business_carriage.rb
+++ app/models/business_carriage.rb
@@ -1,4 +1,4 @@
-class BusinessClassCarriage < Carriage
+class BusinessCarriage < Carriage
   validates :bottom_seats, numericality: { only_integer: true, greater_than: 0 }
 
   def self.permited_params
