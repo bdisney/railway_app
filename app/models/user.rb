99c7a636c95239cc75ab28b635daf254207838bc
disne_y <bold.disney@gmail.com>
2017-03-22 13:29:09 +0300

    Set devise for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :confirmable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :tickets
 
   validates :last_name, presence: true
