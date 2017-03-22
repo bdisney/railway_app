3121eee2eb075b3facc5eb8f9f57fc6ced8ac342
disne_y <bold.disney@gmail.com>
2017-03-22 17:04:27 +0300

    Add admin role to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322102527) do
+ActiveRecord::Schema.define(version: 20170322125636) do
 
   create_table "carriages", force: :cascade do |t|
     t.string   "type"
@@ -75,12 +75,12 @@ ActiveRecord::Schema.define(version: 20170322102527) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20170322102527) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
